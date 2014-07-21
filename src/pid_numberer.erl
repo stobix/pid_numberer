91d1f83c6c032d164ea61fe66e8e2785990621b8
Joel Ericson <joel.mikael.ericson@gmail.com>
2014-07-21 19:40:32 +0200

    initial commit

---
+++ src/pid_numberer.erl
@@ -0,0 +1,178 @@
+%%%-------------------------------------------------------------------
+%%% @author Joel Ericson
+%%% @copyright (C) 2014, Joel Ericson
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2014-06-17 15:16:20.607252
+%%%-------------------------------------------------------------------
+%%% This is a special process. A special process is aware of system  
+%%% messages and also implements some standard functions needed (and  
+%%% standarized) in an OTP environment, so it can be supervised  
+%%% (started/stop), upgraded in a hot-upgrade fashion, etc.  
+%%%  
+%%% See http://www.erlang.org/doc/design_principles/spec_proc.html  
+%%% For system messages, see: http://www.erlang.org/doc/man/sys.html  
+-module(pid_numberer).  
+%%-------------------------------------------------------------------  
+%% API Function Exports  
+%%-------------------------------------------------------------------  
+-export([start_link/0, init/1]).  
+  
+%%-------------------------------------------------------------------  
+%% Required OTP Exports  
+%%-------------------------------------------------------------------  
+-export([  
+    system_code_change/4, system_continue/3,  
+    system_terminate/4, write_debug/3
+]).  
+
+-export([
+    bind/0,bind/1,
+    register/1,
+    reg_name/1
+    ]).
+  
+%%-------------------------------------------------------------------  
+%% API Function Definitions  
+%%-------------------------------------------------------------------  
+%% @private
+%% @doc Starts a new process synchronously. Spawns the process and  
+%%% waits for it to start.  
+%% See http://www.erlang.org/doc/man/proc_lib.html  
+start_link() ->  
+    proc_lib:start_link(?MODULE, init, [self()]).  
+  
+%%-------------------------------------------------------------------  
+%% API Function Definitions  
+%%-------------------------------------------------------------------  
+%% @private
+%% @doc Notifies the parent of a successful start and then runs the  
+%% main loop. When the process has started, it must call  
+%% init_ack(Parent,Ret) or init_ack(Ret), where Parent is the  
+%% process that evaluates this function (see start_link/0 above).  
+%% At this time, Ret is returned.  
+init(Parent) ->  
+    register(?MODULE, self()),  
+    process_flag(trap_exit,true),
+    Debug = sys:debug_options([]),  
+    proc_lib:init_ack(Parent, {ok, self()}),  
+    loop(Parent, Debug, []).  
+  
+%%----------------------------------------------
+%% @doc binds a nonbound number from the general pool to the caller pid.
+%% @todo
+%% @spec () -> non_neg_integer()
+%% @end
+%%----------------------------------------------
+
+bind() ->
+    bind('_').
+
+%%----------------------------------------------
+%% @doc binds a non-bound number from the Module pool to the caller pid.
+%% @todo
+%% @spec (Module) -> non_neg_integer()
+%% @end
+%%----------------------------------------------
+
+bind(Module) ->
+    ?MODULE ! {bind,self(),Module,self()},
+    receive
+        {number,Number} ->
+            Number;
+        Error -> Error
+    end.
+
+register(Module) ->
+    Name=reg_name(Module),
+    register(Name,self()),
+    Name.
+        
+reg_name(Module) ->
+    ?MODULE ! {bind,self(),Module,self()},
+    Number=receive
+        {number,N} -> N;
+        Error -> exit(Error)
+    end,
+    list_to_atom(lists:concat([atom_to_list(Module),Number])).
+
+%%-------------------------------------------------------------------  
+%% Internal Functions  
+%%-------------------------------------------------------------------  
+%% @doc Our main loop, designed to handle system messages.  
+loop(Parent, Debug, State) ->  
+    receive  
+        {bind,PID,Module,Return} ->
+            Number = case get(Module) of
+                undefined ->
+                    put(Module,{[],0}),
+                    0;
+                {[],Highest} ->
+                    put(Module,{[],Highest+1}),
+                    Highest+1;
+                {[Free|Freed],Highest} ->
+                    put(Module,{Freed,Highest}),
+                    Free
+                end,
+            case get(PID) of
+                undefined ->
+                    monitor(process,PID),
+                    put(PID,[{Module,Number}]);
+                Bindings ->
+                    put(PID,[{Module,Number}|Bindings])
+            end,
+            Return ! {number,Number},
+            loop(Parent, Debug, State);
+        {'EXIT',Parent,Reason} ->
+            exit(Reason);
+        {'DOWN',_,_,Child,_} ->
+            case get(Child) of
+                undefined ->
+                    loop(Parent, Debug, State);
+                Bindings ->
+                    Unbind = 
+                        fun({Module,Number}) ->
+                            case get(Module) of
+                                undefined ->
+                                    ok;
+                                {_,0} ->
+                                    erase(Module);
+                                {Freed,Highest} when Number == Highest ->
+                                    put(Module,{Freed,Highest-1});
+                                {Freed,Highest} ->
+                                    put(Module,{[Number|Freed],Highest})
+                            end
+                        end,
+                    lists:map(Unbind,Bindings),
+                    erase(Child),
+                    loop(Parent, Debug, State)
+            end;
+        {system, From, Request} ->  
+            sys:handle_system_msg(  
+                Request, From, Parent, ?MODULE, Debug, State
+            );  
+        Msg ->  
+            sys:handle_debug(  
+                Debug, fun ?MODULE:write_debug/3, ?MODULE, {in, Msg}  
+            ),  
+            loop(Parent, Debug, State)  
+    end.  
+  
+%% @doc Called by sys:handle_debug().  
+write_debug(Dev, Event, Name) ->  
+    io:format(Dev, "~p event = ~p~n", [Name, Event]).  
+  
+%% @doc http://www.erlang.org/doc/man/sys.html#Mod:system_continue-3  
+system_continue(Parent, Debug, State) ->  
+    loop(Parent, Debug, State).  
+  
+%% @doc http://www.erlang.org/doc/man/sys.html#Mod:system_terminate-4  
+system_terminate(Reason, _Parent, _Debug, _State) ->  
+    exit(Reason).  
+  
+%% @doc http://www.erlang.org/doc/man/sys.html#Mod:system_code_change-4  
+system_code_change(State, _Module, _OldVsn, _Extra) ->  
+    {ok, State}.  
+
+
